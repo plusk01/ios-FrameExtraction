c14404e5bf70e2927f2787188f9e87ba59d0458a
Parker Lusk <parkerclusk@gmail.com>
2018-03-31 10:31:26 -0600

    Initial Commit

---
+++ FrameExtraction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FrameExtraction
+//
+//  Created by Parker Lusk on 3/31/18.
+//  Copyright © 2018 Wanderlusk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
